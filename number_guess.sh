a1e4147645794d6959c0c2d3dc0e8cc274cdf266
Semeh Ameur <semeh.ameur@gmail.com>
2025-02-14 15:03:03 +0000

    feat: welcome message, and fetching user

--- number_guess.sh
+++ number_guess.sh
@@ -4,5 +4,19 @@ PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 RANDOM_NUMBER=$((RANDOM % 1000 +1))
 echo "$RANDOM_NUMBER"
+
 echo -e "\nEnter your username:"
 read USERNAME
+
+echo "$($PSQL "SELECT * FROM users;")" | while read USER_ID BAR USER_NAME BAR GAME_PLAYED BAR BEST_GAME
+do
+  if [[ -z $USER_NAME ]]
+  then
+    USER_NAME=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+    echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+    USERNAME=$USER_NAME
+    echo -e "\nWelcome back, $USERNAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
+  
+done
