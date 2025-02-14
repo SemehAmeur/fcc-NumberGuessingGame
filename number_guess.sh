4c23010630360539c780682a7aadb5da02afd981
Semeh Ameur <semeh.ameur@gmail.com>
2025-02-14 14:49:16 +0000

    feat: connect to db

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+
 RANDOM_NUMBER=$((RANDOM % 1000 +1))
 echo "$RANDOM_NUMBER"
 echo -e "\nEnter your username:"
